7f27e6b7831e96cea1ac7a513902090f36757b1a
amir <amirnobari1990@gmail.com>
2024-02-20 18:08:24 +0330

    chore : "create handler for core app"

---
+++ src/handler/commandHandler.ts
@@ -0,0 +1,19 @@
+import { startFramework } from "../commands/start.ts"
+import { stopFramework } from "../commands/stop.ts"
+import { displayHelp } from "../commands/help.ts"
+import { displayStatus } from "../commands/status.ts"
+
+// Function to handle different commands
+export async function handleCommand(args: any) {
+    if (args._.includes("start")) {
+        startFramework()
+    } else if (args._.includes("stop")) {
+        stopFramework()
+    } else if (args["help"] || args["_"].includes("--help")) {
+        displayHelp()
+    } else if (args._.includes("status")) {
+        displayStatus()
+    } else {
+        console.log("Invalid command or missing arguments. Use --help for usage information.")
+    }
+}
